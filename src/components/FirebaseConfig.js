336f02007677b6af21627984a0a1ca27c6d477b8
Micheal Dunne <michealdunne14@gmail.com>
2020-03-20 18:49:16 +0000

    Delete FirebaseConfig.js

--- src/components/FirebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: 'AIzaSyCxhGZkX2X0Xy77elhlChN-K_2xYKer72s',
-  authDomain: 'webdev-c06a8.firebaseapp.com',
-  databaseURL: 'https://webdev-c06a8.firebaseio.com',
-  projectId: 'webdev-c06a8',
-  storageBucket: 'webdev-c06a8.appspot.com',
-  messagingSenderId: '26913515277'
-}
